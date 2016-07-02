e81e104c5bc3aad78054cd59679ff1a1b8549e16
Duha-Che <duha.che@mail.ru>
2016-07-02 11:52:41 +0300

    Commit 5 fils

---
+++ MVC.Chart/MVC.Chart/CommonData/Names.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Chart.CommonData
+{
+    public static class Names
+    {
+        public const string HomePage = "Домашняя страницa";
+        public const string ApplicationName = "Чат";
+        public const string Registration = "Регистрация";
+        public const string Enter = "Выполнить вход";
+    }
+}
---
+++ MVC.Chart/MVC.Chart/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("MVC.Chart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVC.Chart")]
+[assembly: AssemblyCopyright("© , 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("b828573c-f80f-4244-8552-f8a6848c43af")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
